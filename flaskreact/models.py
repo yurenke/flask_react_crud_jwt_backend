f9b800d88a2573a39dd71edc54fe3ee60b19c9e0
yurenke <yuren.ke@gmail.com>
2023-11-04 01:30:52 +0800

    clear comments in models.py

--- flaskreact/models.py
+++ flaskreact/models.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import desc, Index
+from sqlalchemy import Index
 from datetime import datetime
 from flask_migrate import Migrate
-# from flaskreact.ts_vector import TSVector
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import func
 from sqlalchemy import cast, literal
@@ -19,7 +18,6 @@ def create_tsvector(*args):
         exp += ' ' + e
 
     return func.to_tsvector(cast(literal("english"), type_=REGCONFIG), exp)
-    # return func.to_tsvector('english', exp)
   
 class Base(DeclarativeBase):
   pass
@@ -29,7 +27,6 @@ migrate = Migrate()
 
 class Account(db.Model):
     __tablename__ = "accounts"
-    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
     name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
@@ -49,12 +46,6 @@ class Post(db.Model):
     create_time = db.Column(db.DateTime, default=datetime.now)
     update_time = db.Column(db.DateTime, onupdate=datetime.now, index=True, default=datetime.now)
     author_id = db.Column(db.String(32), db.ForeignKey('accounts.id'), nullable=False)
-    # author = db.relationship('Account', backref='posts')
-    # author = db.relationship('Account', backref='posts', lazy='joined')
-
-    # __ts_vector__ = db.Column(TSVector(),db.Computed(
-    #      "to_tsvector('english', title || ' ' || content)",
-    #      persisted=True))
     __ts_vector__ = create_tsvector(
         cast(func.coalesce(title, ''), postgresql.TEXT),
         cast(func.coalesce(content, ''), postgresql.TEXT)
